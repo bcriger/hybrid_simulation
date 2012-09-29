668f2384a957ecceaaa1b065237f9549754add11
Ben Criger <bcriger@gmail.com>
2012-09-28 23:41:42 -0400

    Started class definition for hybrid engines in New Code

---
+++ New Code/HybridEngine.m
@@ -0,0 +1,45 @@
+classdef HybridEngine
+    %HYBRIDENGINE This class is meant to encapsulate all the aspects of a
+    %designed hybrid engine, and methods to calculate derived properties.
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
+classdef Nozzle
+    %NOZZLE This class handles rocket nozzles.
+   
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
+classdef OxidizerTank
+    %OXIDIZERTANK This class handles hybrid rocket oxidizer tanks.
+   
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
+classdef CombustionChamber
+    %OXIDIZERTANK This class handles hybrid rocket oxidizer tanks.
+   
+    properties
+    end
+    
+    methods
+    end
+    
+end
